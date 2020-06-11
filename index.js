f5134ed2b75a5baedd2d3a499174639569eef266
Joerg Roth <joerg.roth@geo.uzh.ch>
2020-06-11 12:51:22 +0200

    a lot of improvements

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { getCircle } from './lib/circle-spheric.mjs';
+import { getGreatCircle } from './lib/greatCircle.mjs';
+import { getGeodesicGrid } from './lib/grid-geodesic.mjs';
+import { getInitialValues } from './lib/geoTools.mjs';
+
+export { getCircle };
+export { getInitialValues };
+export { getGreatCircle };
+export { getGeodesicGrid };
--- leylines-geodesy.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// import modules
-import { points, grid, fibonacci } from './lib/loxodrome.mjs';
-import { geogrid } from './lib/geodesic.mjs';
-import { greatcircles, circle } from './lib/circles.mjs';
-import { calculatePlatonic } from './lib/platonics.mjs';
-import { createCzmlHeader } from './lib/czml.mjs';
-
